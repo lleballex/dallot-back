0ae1a992f22a0f2036003d4a3038f6d34661de49
lleballex <lleballex@yandex.ru>
2020-08-20 08:28:13 +0300

    Remove media urls

--- src/dallot/urls.py
+++ src/dallot/urls.py
@@ -1,7 +1,5 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,4 +7,4 @@ urlpatterns = [
 
     path('api/account/', include('account.urls')),
     path('api/posts/', include('posts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
